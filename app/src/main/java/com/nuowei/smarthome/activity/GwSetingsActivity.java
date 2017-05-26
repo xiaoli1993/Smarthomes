f3177c2473be8522dab9d65ca4c1213b4b84c1eb
xiaoli <554674787@qq.com>
2017-05-26 18:14:16 +0800

    2017-5-26

---
+++ app/src/main/java/com/nuowei/smarthome/activity/GwSetingsActivity.java
@@ -0,0 +1,23 @@
+package com.nuowei.smarthome.activity;
+
+import android.os.Bundle;
+
+import com.nuowei.smarthome.R;
+
+/**
+ * @Author : 肖力
+ * @Time :  2017/5/26 13:58
+ * @Description :
+ * @Modify record :
+ */
+
+public class GwSetingsActivity extends BaseActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gw_setings);
+//        initEven();
+    }
+
+}
